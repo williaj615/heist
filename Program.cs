a6c602c0101442624b9a9d99790e009032ed518e
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-03-02 13:00:29 -0600

    completed phase two

--- Program.cs
+++ Program.cs
@@ -7,31 +7,50 @@ namespace heist
   {
     static void Main(string[] args)
     {
+
+      List<Dictionary<string, string>> teamMembers = new List<Dictionary<string, string>>();
+
       Console.WriteLine("Plan Your Heist!");
 
-      Dictionary<string, string> teamMember = new Dictionary<string, string>();
 
-      Console.WriteLine("Enter Your Team Member's name.");
-
-      string memberName = Console.ReadLine();
-
-      teamMember.Add("Name", memberName);
-
-      Console.WriteLine("Thanks! Now, enter your team member's skill level.");
-
-      string memberLevel = Console.ReadLine();
-      //   int memberLevelInt = int.Parse(memberLevel);
-      teamMember.Add("Skill Level", memberLevel);
-
-      Console.WriteLine("Awesome! Now, enter your team member's courage factor.");
-
-      string memberFactor = Console.ReadLine();
-      //   int memberFactorInt = int.Parse(memberFactor);
-      teamMember.Add("Courage Factor", memberFactor);
-
-      foreach (KeyValuePair<string, string> pair in teamMember)
+      while (true)
       {
-        Console.WriteLine($"The team member's {pair.Key} is {pair.Value}");
+        Dictionary<string, string> teamMember = new Dictionary<string, string>();
+
+        Console.WriteLine("Enter Your Team Member's name.");
+
+        string memberName = Console.ReadLine();
+        if (memberName == "")
+        {
+          break;
+        }
+
+        teamMember.Add("Name", memberName);
+
+        Console.WriteLine("Thanks! Now, enter your team member's skill level.");
+
+        string memberLevel = Console.ReadLine();
+        //   int memberLevelInt = int.Parse(memberLevel);
+        teamMember.Add("Skill Level", memberLevel);
+
+        Console.WriteLine("Awesome! Now, enter your team member's courage factor.");
+
+        string memberFactor = Console.ReadLine();
+        //   int memberFactorInt = int.Parse(memberFactor);
+        teamMember.Add("Courage Factor", memberFactor);
+
+        teamMembers.Add(teamMember);
+      }
+
+      Console.WriteLine($"Sweet! You have created a team with {teamMembers.Count} members.");
+
+      foreach (Dictionary<string, string> member in teamMembers)
+      {
+
+        foreach (KeyValuePair<string, string> pair in member)
+        {
+          Console.WriteLine($"The team member's {pair.Key} is {pair.Value}");
+        }
       }
     }
   }
